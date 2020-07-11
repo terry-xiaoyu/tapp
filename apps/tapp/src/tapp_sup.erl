4da9ebaf7f4b18214e9a8163bef4b7f1be587b2f
terry-xiaoyu <506895667@qq.com>
2020-07-11 10:19:17 +0800

    Add an server

--- apps/tapp/src/tapp_sup.erl
+++ apps/tapp/src/tapp_sup.erl
@@ -29,7 +29,14 @@ init([]) ->
     SupFlags = #{strategy => one_for_all,
                  intensity => 0,
                  period => 1},
-    ChildSpecs = [],
+    ChildSpecs = [
+        #{id => tserver,
+          start => {tserver, start_link, []},
+          restart => temporary,
+          shutdown => 3000,
+          type => worker,
+          modules => [tserver]}
+    ],
     {ok, {SupFlags, ChildSpecs}}.
 
 %% internal functions
